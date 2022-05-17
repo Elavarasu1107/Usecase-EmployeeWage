11511800b6117e09d292a36336ba9efa1d42e5b2
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-17 10:19:06 +0530

    [Elavarasu] UC6-WageHours

--- Employee/Employee/EmpAttendance.cs
+++ Employee/Employee/EmpAttendance.cs
@@ -10,38 +10,44 @@ namespace Employee
     {
         public void WageForMonth()
         {
-            int empWagePerHour = 20, empWorkDaysMonth=20, empHours, dailyWage;
-            int totalWorkingDays = 0,totalSalary=0,salaryForDay;
+            int empWagePerHour = 20, empWorkDaysPerMonth=20, empHours, salaryForDay;
+            int totalWorkingDays=0, totalSalary=0,totalWorkingHours=0;
             Random value=new Random();
 
-            for(int days=1;days< empWorkDaysMonth; days++)
+            for(int i=totalWorkingDays; i < empWorkDaysPerMonth; i++)
             {
-                int randomCheck = value.Next(3);
-               // Console.WriteLine("Generated random value is:" + randomCheck);
+               if(i< empWorkDaysPerMonth && totalWorkingHours<100)
+               {
+                    int randomCheck = value.Next(3);
+                    //Console.WriteLine("Generated random value is:" + randomCheck);
 
-                switch (randomCheck)
-                {
-                    case 1:// For Part Time
-                        {
-                            empHours = 4;
-                            break;
-                        }
-                    case 2: // For Full Time
-                        {
-                            empHours = 8;
-                            break;
-                        }
-                    default: // For Absent
-                        {
-                            empHours = 0;
-                            break;
-                        }     
-                }
-                salaryForDay = empHours * empWagePerHour;
-                totalSalary = totalSalary + salaryForDay;
+                    switch (randomCheck)
+                    {
+                        case 1:// For Part Time
+                            {
+                                empHours = 4;
+                                break;
+                            }
+                        case 2: // For Full Time
+                            {
+                                empHours = 8;
+                                break;
+                            }
+                        default: // For Absent
+                            {
+                                empHours = 0;
+                                break;
+                            }
+                    }
+                    totalWorkingDays = i+1;
+                    salaryForDay = empHours * empWagePerHour;
+                    totalSalary = totalSalary + salaryForDay;
+                    totalWorkingHours=totalWorkingHours + empHours;
+               }
             }
-                Console.WriteLine("Monthly salary of an Employee is:" + totalSalary);
-
+            Console.WriteLine("Total Working Days of an Employee is:" + totalWorkingDays);
+            Console.WriteLine("Total Working Hours of an Employee is:" + totalWorkingHours);
+            Console.WriteLine("Monthly salary of an Employee is:" + totalSalary);
         }
     }
 }
